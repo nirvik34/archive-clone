65e474a32e66fdb9f9f46d924c4866a5109923e8
nirvik34 <jarhnirvik@gmail.com>
2025-05-04 19:43:20 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/">Rewind</Link>
       </div>
-      <ul className="navbar-links">
-        <li><Link to="/about">About</Link></li> {/* Link to the About page */}
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <ul className={`navbar-links ${isOpen ? "show" : ""}`}>
+        <li><Link to="/about">About</Link></li>
         <li><Link to="/blog">Blog</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
-      <div className="navbar-buttons">
+
+      <div className={`navbar-buttons ${isOpen ? "show" : ""}`}>
         <Link to="/signup">
           <button className="btn btn-signup">Sign up</button>
         </Link>
